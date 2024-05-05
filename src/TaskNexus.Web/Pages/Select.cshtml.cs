f44b36f795aada890be9e39ebfde745517468f9e
Саня <52768975+Zeroooy@users.noreply.github.com>
2024-05-05 09:30:59 +0300

    try to make chat

---
+++ src/TaskNexus.Web/Pages/Select.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskNexus.Web.Pages
+{
+    public class SelectModel : PageModel
+    {
+        private readonly ILogger<SelectModel> _logger;
+
+        public SelectModel(ILogger<SelectModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
